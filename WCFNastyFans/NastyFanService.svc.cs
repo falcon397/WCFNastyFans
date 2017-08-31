46a4c2e70dc5a4022798edc7b069eb7c67880c11
falcon397 <michaelhuckleberry.mh@gmail.com>
2017-08-30 17:51:48 -0700

    Just tested in IIS, compiles and localhost IIS is configured, felt like a good time to take a snapshot of everything working.

--- WCFNastyFans/NastyFanService.svc.cs
+++ WCFNastyFans/NastyFanService.svc.cs
@@ -9,9 +9,9 @@ using MySql.Data.MySqlClient;
 
 namespace WCFNastyFans
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : INastyFans
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "NastyFanServic" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select NastyFanServic.svc or NastyFanServic.svc.cs at the Solution Explorer and start debugging.
+    public class NastyFanServic : INastyFans
     {
         public string GetBuysData()
         {
